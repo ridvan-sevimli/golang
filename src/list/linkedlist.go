fa793b69d9f52adbacff5b28cdf0f9e68dec1d70
ridvan-sevimli <fencing_04_den@icloud.com>
2024-10-23 12:04:41 +0200

    Add Get, set removeFirst, removeLast getFirst getLast remove methods

--- src/list/linkedlist.go
+++ src/list/linkedlist.go
@@ -20,7 +20,7 @@ func New[T any]() *LinkedList[T] {
 
 // Methods
 
-// add(E element) - Adds an element at the end of the list
+// Add(E element) - Adds an element at the end of the list
 func (l *LinkedList[T]) Add(value T) {
 	newNode := node[T]{value: &value, next: nil}
 	node := l.root
@@ -35,8 +35,8 @@ func (l *LinkedList[T]) Add(value T) {
 	node.next = &newNode
 }
 
-// addAt (int index, E value)
-func (l *LinkedList[T]) addAt(index int, value T) {
+// AddAt (int index, E value)
+func (l *LinkedList[T]) AddAt(index int, value T) {
 	if hasNext(l.root) {
 		count := 0
 		nodez := l.root.next
@@ -54,8 +54,8 @@ func (l *LinkedList[T]) addAt(index int, value T) {
 	}
 }
 
-// get(int index)
-func (l LinkedList[T]) get(index int) T {
+// Get(int index)
+func (l LinkedList[T]) Get(index int) T {
 	var zero T
 	if hasNext(l.root) {
 		counter := 0
@@ -72,12 +72,65 @@ func (l LinkedList[T]) get(index int) T {
 	return zero
 }
 
-// set(int index, E element) - Replaces the element tat the specified poistion with the specifeid element
-// removeFirst()
-// removeLast()
-// getFirst()
-// getLast()
-// remove(int index) - Removes the element at the specified index
+// Set(index int, E element) - Replaces the element that the specified poistion with the specifeid element
+func (l *LinkedList[T]) Set(index int, value T) bool {
+	if hasNext(l.root) {
+		counter := 0
+		node := l.root.next
+		for index > 0 && hasNext(node) {
+			if counter == index {
+				node.value = &value
+				return true
+			}
+			node = node.next
+			counter++
+		}
+	}
+	return false
+}
+
+// RemoveFirst()
+func (l *LinkedList[T]) RemoveFirst() {
+	if hasNext(l.root) {
+		l.root.next = l.root.next.next
+	}
+}
+
+// RemoveLast()
+func (l *LinkedList[T]) RemoveLast() {
+	if hasNext(l.root) {
+		node := l.root.next
+		for hasNext(node) {
+			node = node.next
+		}
+		node.next = nil
+	}
+}
+
+// GetFirst()
+func (l *LinkedList[T]) GetFirst() T {
+	var zero T
+	if hasNext(l.root) {
+		return *l.root.next.value
+	}
+	return zero
+}
+
+// GetLast()
+func (l *LinkedList[T]) GetLast() T {
+	var zero T
+	if hasNext(l.root) {
+		node := l.root.next
+		for hasNext(node) {
+			node = node.next
+		}
+		return *node.value
+	}
+	return zero
+}
+
+// Remove(int index) - Removes the element at the specified index
+
 // remove(Object o) - Removes the first occurrence of the specified element
 // peek() - Retrieves, but does not remove, the head of the list, or teturns null if the list is empty
 
